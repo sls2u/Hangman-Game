4a2da9485d7ca62e81d5e8fd10583f2cd5034896
sls2u <sarahstewartvsp@gmail.com>
2018-03-01 18:15:04 -0500

    added variable, counter, function

--- assets/javascript/gmae.js
+++ assets/javascript/gmae.js
@@ -1,29 +1,88 @@
-//create variables first decare everything you need to keep track up at the top
+//variables
+var selectedLetter="";
+var guessesSoFar = "y";
+var letterOptions = ["a","b","c","d","e","f","g","h","i","j"]
 
-var word = 'popcorn';
-var dashes = [];
-var alreadyGussed = [];
+//counter
+var guessesLeft = 26;
+var wins = 0;
+var losses = 0;
 
+for (var i = 0; i < guessesLeft; i++) {
 
-for (var i =0; i < word.length; i++) {
-  dashes.push('-');
 }
 
-var currentResltEL = document.getElementById("current-result");
-var guessesDisplayEL = document.getElementByID('previous-guesses');
+function startGame() {
+  selectedLetter = letterOptions[Math.floor(Math.random()*letterOptions.length)];
+  console.log(selectedLetter);
+  console.log(guessesLeft);
+}
 
-currentResultEl.innerText = dashes.join('');
+function
 
-document.onkeyup = function(event) {
-  var key = event.key;
 
-if (alreadyBuessed.index(key)
 
-  alreadyGussed.push(key);
-  gussessDisplayEL.innerText = alreadyBuessed.join('');
-  console.log("alreadyGussed") !==-1) {
-    return false;
-  }
-.split (' ').join(' ')
 
-//indexof
+
+
+
+
+
+
+
+
+
+startGame();
+
+
+
+
+// var person = "player";
+// console.log (person);
+//
+// var alreadyGuessed = []
+// var wordLength =[]
+//
+// var wordChosen =[]
+//
+// var counter = 'x'--1;
+//
+// var wordBank = new Array;
+// var currentLetterGuessed;
+//
+//
+//
+
+
+
+
+
+//create variables first decare everything you need to keep track up at the top
+
+// var word = 'popcorn';
+// var dashes = [];
+// var alreadyGussed = [];
+//
+//
+// for (var i =0; i < word.length; i++) {
+//   dashes.push('-');
+// }
+//
+// var currentResltEL = document.getElementById("current-result");
+// var guessesDisplayEL = document.getElementByID('previous-guesses');
+//
+// currentResultEl.innerText = dashes.join('');
+//
+// document.onkeyup = function(event) {
+//   var key = event.key;
+//
+// if (alreadyBuessed.index(key)
+//
+//   alreadyGussed.push(key);
+//   gussessDisplayEL.innerText = alreadyBuessed.join('');
+//   console.log("alreadyGussed") !==-1) {
+//     return false;
+//   }
+// .split (' ').join(' ')
+//
+// //indexof

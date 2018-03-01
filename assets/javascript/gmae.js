c5032afa21e6e6ea81803f14984f3cbfa6bcfe06
sls2u <sarahstewartvsp@gmail.com>
2018-02-28 21:56:48 -0500

    created backup file

---
+++ assets/javascript/gmae.js
@@ -0,0 +1,29 @@
+//create variables first decare everything you need to keep track up at the top
+
+var word = 'popcorn';
+var dashes = [];
+var alreadyGussed = [];
+
+
+for (var i =0; i < word.length; i++) {
+  dashes.push('-');
+}
+
+var currentResltEL = document.getElementById("current-result");
+var guessesDisplayEL = document.getElementByID('previous-guesses');
+
+currentResultEl.innerText = dashes.join('');
+
+document.onkeyup = function(event) {
+  var key = event.key;
+
+if (alreadyBuessed.index(key)
+
+  alreadyGussed.push(key);
+  gussessDisplayEL.innerText = alreadyBuessed.join('');
+  console.log("alreadyGussed") !==-1) {
+    return false;
+  }
+.split (' ').join(' ')
+
+//indexof

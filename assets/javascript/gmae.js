bdc46f1c31c25fcee13a7bdd02d30fc1b7ee7cf0
sls2u <sarahstewartvsp@gmail.com>
2018-03-03 17:42:14 -0500

    messup up

--- assets/javascript/gmae.js
+++ assets/javascript/gmae.js
@@ -8,13 +8,14 @@
 
 //variables
  // var selectedLetter="";
-var targetLetter ;//var will be defined later on
-var guessedLetter ; //array for user to choose letter
-var letters = ["a","b","c","d","e","f","g","h","i","j"];
+var targetLetter = "";//var will be defined later on
+var guessedLetter = [] ; //array for user to choose letter
+var letters = ["a","b","c","d","e","f","g","h","i","j"];//computer
 
 
 //counter
 var guessesLeft = 10;
+var guesseSoFar =[];
 var wins = 0;
 var losses = 0;
 var i = 0;
@@ -22,46 +23,49 @@ var i = 0;
 
 
 //Display start screen info
-function startGame() {//startgame
+ function startGame() {//startgame
   guessesLeft = 10;//assigns how many guesses to start with
   targetLetter =  letters[Math.floor(Math.random() * letters.length)];//computer choses letter at random to be guessed
   guessedLetter = [];//clear out all letters guessed
   console.log(targetLetter);
+ }
 
-}
+ startGame();
+
 
   document.onkeyup = function(event) {//function runs when a key is pressed by user
+
     var userGuess = String.fromCharCode(event.keyCode).toLowerCase() //function happens taking in user guess by keypress store info for later use
 //   console.log(userGuess);
-    var computerGuess = letters[Math.floor(Math.random() * letters.length)];//function for computer pick random letter from array
+    guessedLetter.push(userGuess);//pushing user guess to gussesSoFar
 
     //compare guess against one another to determine results using conditional statements to determine results
-  letters.indexof(userGuess)  !== -1
 
-  if (userGuess == computerGuess) {
+  if (userGuess === targetLetter) {
             wins++;//increments by 1
             alert('You\'ve guesesed corrrectly!');
+            startGame();
+        }
+        else if (userGuess !== targetLetter){
+          guessesLeft--;
+         alert('You didn\'t guess the correct letter. Let\'s try again.');
         }
 
-        else (userGuess !== computerGuess) {
-            guessLeft--; //decrease by 1
-            alert('You didn\'t guess the correct letter. Try again.');
-        }
-      }
 
+        //   }
 
-
-
-    // check results
-
-    // write result to og
+          // } else if (userGuess !== computerGuess){
+          //   guessesLeft--; //decrementing the guesses left
+          //
+          // // } else  {
+          // //  losses++; //increase by 1
+          // //   alert('You didn\'t guess the correct letter. Try again.');
+          // }
+    //
     updatePage();
 }
 
 
-    //         alert:("You\ve guessed correctly" + "Good Job!");
-    //       }
-    // }
 
 
 function updatePage() {//named updatePage to refresh  page function to keep track of wins and losses
@@ -70,90 +74,20 @@ function updatePage() {//named updatePage to refresh  page function to keep trac
     document.getElementById("losses").innerHTML = "losses " + losses; //document user losess pull from html id
 
     document.getElementById("guessesSoFar").innerHTML = "guessesSoFar" + guessesSoFar; // keep track of how many guesses pull from html id
+
+    document.getElementById("guessesLeft").innterHTML = "guessesLeft" + guessesLeft;
 }
 
 
-// if (userGuess == computerGuess) {
-//   wins i++;
-//   alert("You\ve guessed correctly" + "Good Job!");
-//   guessesLeft = 10; //count stays the same
-//
-//
-// // }
-//
-//
-//
-//
-//
-//
-// // }
-// //want computer to generate random letter//
-//
-// function randLetter() {
-//     var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"];
-//     var letter = letters[Math.floor(Math.random() * letters.length)];
-//     console.log(letter);
-//
-// }
-//       $('#letter').html(randLetter())
-//
+
 // // want enter to start game and box to prompt so user can input letter.//
-// function myFunction() {
-//     var user = prompt("Please enter one letter from A-J");
-//     var guess = [];
-//     if (guess!== letter) {
-//       msg = "You Win";
-//     } else {
-//       msg = "Try Again";
-//     }
+//  function myFunction() {
+//   var user = prompt("Please enter one letter from A-J");
 // }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//create variables first decare everything you need to keep track up at the top
-
-// var word = 'popcorn';
-// var dashes = [];
-// var alreadyGussed = [];
-//
-//
-// for (var i =0; i < word.length; i++) {
-//   dashes.push('-');
-// }
-//
-// var currentResltEL = document.getElementById("current-result");
-// var guessesDisplayEL = document.getElementByID('previous-guesses');
-//
-// currentResultEl.innerText = dashes.join('');
-//
-// document.onkeyup = function(event) {
-//   var key = event.key;
-//
-// if (alreadyBuessed.index(key)
-//
-//   alreadyGussed.push(key);
-//   gussessDisplayEL.innerText = alreadyBuessed.join('');
-//   console.log("alreadyGussed") !==-1) {
-//     return false;
-//   }
-// .split (' ').join(' ')
-//
-// //indexof
+//   var guess = [];
+// //     if (guess!== letter) {
+// //       msg = "You Win";
+// //     } else {
+// //       msg = "Try Again";
+// //     }
+// // }

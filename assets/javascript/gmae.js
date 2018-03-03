5d397769216af2bf1a434b2a9fa02527ed5924e8
sls2u <sarahstewartvsp@gmail.com>
2018-03-03 14:23:36 -0500

    line 41-48 added if/else statements

--- assets/javascript/gmae.js
+++ assets/javascript/gmae.js
@@ -1,3 +1,4 @@
+//have output for game for user instructions
 //User makes choice
 //computer makes choice
 //capture choices
@@ -5,13 +6,11 @@
 //display result to the user
 
 
-
 //variables
-// var selectedLetter="";
+ // var selectedLetter="";
 var targetLetter ;//var will be defined later on
 var guessedLetter ; //array for user to choose letter
 var letters = ["a","b","c","d","e","f","g","h","i","j"];
-//
 
 
 //counter
@@ -21,6 +20,7 @@ var losses = 0;
 var i = 0;
 
 
+
 //Display start screen info
 function startGame() {//startgame
   guessesLeft = 10;//assigns how many guesses to start with
@@ -28,42 +28,51 @@ function startGame() {//startgame
   guessedLetter = [];//clear out all letters guessed
   console.log(targetLetter);
 
-
-  updatePage();//calling counter functions
-
 }
 
-function updatePage() {//named updateCounter function to keep track of wins and losses
-    document.getElementById("wins").innerHTML = "Wins " + wins //documenting if user presses correct letter  pull from html id
+  document.onkeyup = function(event) {//function runs when a key is pressed by user
+    var userGuess = String.fromCharCode(event.keyCode).toLowerCase() //function happens taking in user guess by keypress store info for later use
+//   console.log(userGuess);
+    var computerGuess = letters[Math.floor(Math.random() * letters.length)];//function for computer pick random letter from array
 
-    document.getElementById("losses").innerHTML = "Losses " + losses; //document user losess pull from html id
+    //compare guess against one another to determine results using conditional statements to determine results
+  letters.indexof(userGuess)  !== -1
+
+  if (userGuess == computerGuess) {
+            wins++;//increments by 1
+            alert('You\'ve guesesed corrrectly!');
+        }
+
+        else (userGuess !== computerGuess) {
+            guessLeft--; //decrease by 1
+            alert('You didn\'t guess the correct letter. Try again.');
+        }
+      }
+
+
+
+
+    // check results
+
+    // write result to og
+    updatePage();
+}
+
+
+    //         alert:("You\ve guessed correctly" + "Good Job!");
+    //       }
+    // }
+
+
+function updatePage() {//named updatePage to refresh  page function to keep track of wins and losses
+    document.getElementById("wins").innerHTML = "wins " + wins //documenting if user presses correct letter  pull from html id
+
+    document.getElementById("losses").innerHTML = "losses " + losses; //document user losess pull from html id
 
     document.getElementById("guessesSoFar").innerHTML = "guessesSoFar" + guessesSoFar; // keep track of how many guesses pull from html id
-
-}
-function updatePage(letters) { //letter called when a guess happens
-    guessesLeft = guessedLetter.push(letters) - 1; //adding new item to the end of an array, and returning the new length.
-
-
- }
-
-function updatePage(letters) {//letter called when a guess happens
-  guessessofar = guessedLetter.push(letters) + 1;  //adding new item to the end of an array, and returning the new length
-
-  updatePage();
 }
 
 
-
-// document.onkeyup = function() {
-//   var userGuess = String.fromCharCode(event.keyCode).toLowerCase();//taking in user guess
-//   console.log(userGuess);
-//
-//
-//   var computerGuess = letters[Math.floor(Math.random() * letters.length)];//function for computer pick random letter
-//
-// //   console.log(letter);
-//
 // if (userGuess == computerGuess) {
 //   wins i++;
 //   alert("You\ve guessed correctly" + "Good Job!");
